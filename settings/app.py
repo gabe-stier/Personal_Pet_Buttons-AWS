e602b7f214e82aeaa1058426f5174e24862a298a
Gabe Stier <37159135+gabe-stier@users.noreply.github.com>
2023-05-16 20:06:53 -0500

    Working on connection between dynamodb and lambda

--- settings/app.py
+++ settings/app.py
@@ -1,39 +1,52 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
+import logging
+import os
+import pprint
+import time
 
-# client = boto3.client("dynamodb")
+client = boto3.resource("dynamodb")
 
+settings_keys = {'quietTime', 'quietButtons', "sendAudio", "sendNotifications"}
+
+def get_settings(retrieved):
+    tries = 0
+    max_tries = 5
+    sleepy_time = 1 
+    while tries < max_tries:
+        response = client.batch_get_item(RequestItems=settings_keys)
+        # Collect any retrieved items and retry unprocessed keys.
+        for key in response.get('Responses', []):
+            retrieved[key] += response['Responses'][key]
+        unprocessed = response['UnprocessedKeys']
+        if len(unprocessed) > 0:
+            batch_keys = unprocessed
+            unprocessed_count = sum(
+                [len(batch_key['Keys']) for batch_key in batch_keys.values()])
+            logger.info(
+                "%s unprocessed keys returned. Sleep, then retry.",
+                unprocessed_count)
+            tries += 1
+            if tries < max_tries:
+                logger.info("Sleeping for %s seconds.", sleepy_time)
+                time.sleep(sleepy_time)
+                sleepy_time = min(sleepy_time * 2, 32)
+        else:
+            break
+    return retrieved
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    print(event)
 
     response_code = 200
 
+    retrieved = {key: [] for key in settings_keys}
+    retrieved = get_settings(retrieved)
     return_body = {
         "method": event['httpMethod'],
         "event": event,
-        "body": json.loads(event['body'])
+        "body": json.loads(event['body']),
+        "data": retrieved
     }
 
     if event['httpMethod'] == "PUT":
